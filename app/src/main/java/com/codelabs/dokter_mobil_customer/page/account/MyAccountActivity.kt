0bb96d16079c687f8a10a339d0b8cf9c4f168709
Rudi Tawangga <ruditawangga@gmail.com>
2021-02-26 14:03:00 +0700

    coba

--- app/src/main/java/com/codelabs/dokter_mobil_customer/page/account/MyAccountActivity.kt
+++ app/src/main/java/com/codelabs/dokter_mobil_customer/page/account/MyAccountActivity.kt
@@ -1,6 +1,5 @@
 package com.codelabs.dokter_mobil_customer.page.account
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codelabs.dokter_mobil_customer.R
 import com.codelabs.dokter_mobil_customer.connection.ApiUtils
@@ -8,12 +7,9 @@ import com.codelabs.dokter_mobil_customer.connection.AppConstant
 import com.codelabs.dokter_mobil_customer.connection.DataManager
 import com.codelabs.dokter_mobil_customer.connection.ErrorUtils
 import com.codelabs.dokter_mobil_customer.helper.BaseActivity
-import com.codelabs.dokter_mobil_customer.viewmodel.PrivacyPolicy
 import com.codelabs.dokter_mobil_customer.viewmodel.Profile
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_my_account.*
-import kotlinx.android.synthetic.main.activity_setting.*
-import kotlinx.android.synthetic.main.activity_setting.iv_back
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -35,9 +31,9 @@ class MyAccountActivity : BaseActivity() {
                 if (data.isSuccessful) {
                     val response = data.body()
                     if (data.code() == 200) {
-                        tv_name.text = response?.data?.customerName
-                        tv_email.text = response?.data?.customerEmail
-                        Picasso.get().load(response?.data?.image).into(iv_photo)
+                        tv_name.text = response?.dataProfile?.customerName
+                        tv_email.text = response?.dataProfile?.customerEmail
+                        Picasso.get().load(response?.dataProfile?.image).into(iv_photo)
                     }
                 } else {
                     val error = ErrorUtils.parseError(data)

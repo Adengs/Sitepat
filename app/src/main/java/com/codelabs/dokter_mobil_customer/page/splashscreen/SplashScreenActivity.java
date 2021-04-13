3c347165aca804fab55764e1c0e3256a43d892e0
Rudi Tawangga <ruditawangga@gmail.com>
2021-04-13 09:57:28 +0700

    login fesbuk

--- app/src/main/java/com/codelabs/dokter_mobil_customer/page/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/codelabs/dokter_mobil_customer/page/splashscreen/SplashScreenActivity.java
@@ -1,8 +1,12 @@
 package com.codelabs.dokter_mobil_customer.page.splashscreen;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Base64;
 
 import androidx.annotation.NonNull;
 
@@ -21,12 +25,16 @@ import com.codelabs.dokter_mobil_customer.utils.RecentUtils;
 import com.codelabs.dokter_mobil_customer.viewmodel.DataLogin;
 import com.codelabs.dokter_mobil_customer.viewmodel.GetToken;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import android.util.Base64;
+import android.util.Log;
 
 public class SplashScreenActivity extends BaseActivity {
 
@@ -38,6 +46,20 @@ public class SplashScreenActivity extends BaseActivity {
         setContentView(R.layout.activity_splash_screen);
         loadAppToken();
         fetchData();
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.codelabs.dokter_mobil_customer",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        }
+        catch (PackageManager.NameNotFoundException e) {
+        }
+        catch (NoSuchAlgorithmException e) {
+        }
     }
 
     private void fetchData() {

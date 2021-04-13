cb5fe068057ebd483c1a85134309f2278bcf3bc6
ricky <ricky@codelabs.co.id>
2021-04-13 12:32:24 +0700

    add layout service record detail

--- app/src/main/java/com/codelabs/dokter_mobil_customer/page/account/DetailCardActivity.kt
+++ app/src/main/java/com/codelabs/dokter_mobil_customer/page/account/DetailCardActivity.kt
@@ -3,10 +3,12 @@ package com.codelabs.dokter_mobil_customer.page.account
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.codelabs.dokter_mobil_customer.R
+import kotlinx.android.synthetic.main.activity_detail_card.*
 
 class DetailCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_card)
+        iv_close_card.setOnClickListener { finish() }
     }
 }
